77032040da4b19a89f88f0a190d37f8c51dfaeae
孙皓 <shsgear@qq.com>
2018-12-28 18:37:53 +0800

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    // Read candidate
+    string public candidate;
+    // Save candidate
+    // Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
